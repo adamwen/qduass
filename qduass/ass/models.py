d3f836404dc94488b42f4e5e8e27c0893f37b7b3
Adam Wen <adamwen829@gmail.com>
2013-03-15 19:06:54 +0800

    fix spell bug

--- qduass/ass/models.py
+++ qduass/ass/models.py
@@ -5,10 +5,10 @@ class User(models.Model):
     openid = models.CharField(max_length=70)
     openkey = models.CharField(max_length=70)
 
-    accesstoken = models.CharField(max_lenght=70)
-    expires_in = models.CharField(max_lenght=70)
-    refresh_key = models.CharField(max_lenght=70)
+    accesstoken = models.CharField(max_length=70)
+    expires_in = models.CharField(max_length=70)
+    refresh_key = models.CharField(max_length=70)
 
-    nickname = models.CharField(max_lenght=70)
-    name = models.CharField(max_lenght=70)
+    nickname = models.CharField(max_length=70)
+    name = models.CharField(max_length=70)
 
