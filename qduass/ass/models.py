d727e0fe4f2b950e13741008f074e5f7444658f9
Adam Wen <adamwen829@gmail.com>
2013-03-15 19:05:08 +0800

    fix the charfield limit

--- qduass/ass/models.py
+++ qduass/ass/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    openid = models.CharField(max_length=700)
-    openkey = models.CharField(max_length=700)
+    openid = models.CharField(max_length=70)
+    openkey = models.CharField(max_length=70)
 
-    accesstoken = models.CharField(max_lenght=700)
-    expires_in = models.CharField(max_lenght=700)
-    refresh_key = models.CharField(max_lenght=700)
+    accesstoken = models.CharField(max_lenght=70)
+    expires_in = models.CharField(max_lenght=70)
+    refresh_key = models.CharField(max_lenght=70)
 
-    nickname = models.CharField(max_lenght=700)
-    name = models.CharField(max_lenght=700)
+    nickname = models.CharField(max_lenght=70)
+    name = models.CharField(max_lenght=70)
 
