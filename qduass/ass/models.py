373297345d935e4cd0917dc72ac9daaed2d78ded
Adam Wen <adamwen829@gmail.com>
2013-03-15 18:53:38 +0800

    unite indent

--- qduass/ass/models.py
+++ qduass/ass/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-	openid = model.CharField(max_length=70)
-	openkey = model.CharField(max_length=70)
+    openid = model.CharField(max_length=70)
+    openkey = model.CharField(max_length=70)
 
-	accesstoken = model.CharField(max_lenght=70)
-	expires_in = model.CharField(max_lenght=70)
-	refresh_key = model.CharField(max_lenght=70)
+    accesstoken = model.CharField(max_lenght=70)
+    expires_in = model.CharField(max_lenght=70)
+    refresh_key = model.CharField(max_lenght=70)
 
     nickname = model.CharField(max_lenght=70)
     name = model.CharField(max_lenght=70)
